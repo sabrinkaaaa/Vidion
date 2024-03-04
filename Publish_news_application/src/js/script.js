199778738e8a6a1c8f3e92337b628c59150179b5
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2024-03-04 12:24:39 +0700

    Страница "Опубликовать новость по заявке": - изменен дизайн кнопок - добавлено ограничение на количество символов в полях. Поле с суммой доступен ввод только цифр

--- Publish_news_application/src/js/script.js
+++ Publish_news_application/src/js/script.js
@@ -1,27 +1,21 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.publish_news-btn').addEventListener('click', function(event) {
-      var title = document.getElementById('title_news').value.trim();
-      var description = document.querySelector('textarea[name="description_news"]').value.trim();
-      var link = document.getElementById('link_to_news').value.trim();
-      var price = document.getElementById('set_price').value.trim();
+var titleField = document.getElementById('title_news');
+var descriptionField = document.getElementById('description_news');
+var linkField = document.getElementById('link_to_news');
 
-      // Проверка поля цены на наличие только цифр
-      if (!(/^\d+$/.test(price))) {
-        alert('Пожалуйста, введите только цифры в поле цены.');
-        event.preventDefault();
-        return;
-      }
+titleField.addEventListener('input', function() {
+    if (this.value.length > 1000) {
+        this.value = this.value.slice(0, 1000);
+    }
+});
 
-      // Проверка остальных полей на ограничение в 1000 символов
-      if (title.length > 1000 || description.length > 1000 || link.length > 1000) {
-        alert('Поля Название, Описание и Ссылка на материал должны содержать не более 1000 символов.');
-        event.preventDefault();
-        return;
-      }
+descriptionField.addEventListener('input', function() {
+  if (this.value.length > 1000) {
+      this.value = this.value.slice(0, 1000);
+  }
+});
 
-      // Если все проверки прошли успешно, можно отправить форму
-      document.publish_news.submit();
-    });
-  });
-</script>
+linkField.addEventListener('input', function() {
+  if (this.value.length > 1000) {
+      this.value = this.value.slice(0, 1000);
+  }
+});
